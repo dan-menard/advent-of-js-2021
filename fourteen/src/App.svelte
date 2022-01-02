3ae00f4127a7af6ecf97bfcd65a5abeb27fbd675
Dan Menard <daniel.menard@gmail.com>
2022-01-02 14:29:02 -0500

    Problem 14: Add project files.

--- fourteen/src/App.svelte
+++ fourteen/src/App.svelte
@@ -1,30 +1,59 @@
 <script>
-	export let name;
+  export let today;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="wrapper">
+  <div class="previous">
+    <img src="images/previous.svg" alt="Previous" />
+  </div>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  <div class="month">December</div>
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  <div class="next">
+    <img src="images/next.svg" alt="Next" />
+  </div>
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+  <div class="day-of-week">S</div>
+  <div class="day-of-week">M</div>
+  <div class="day-of-week">T</div>
+  <div class="day-of-week">W</div>
+  <div class="day-of-week">T</div>
+  <div class="day-of-week">F</div>
+  <div class="day-of-week">S</div>
+
+  <div></div>
+  <div></div>
+  <div> </div>
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>5</div>
+  <div>6</div>
+  <div>7</div>
+  <div>8</div>
+  <div>9</div>
+  <div>10</div>
+  <div>11</div>
+  <div>12</div>
+  <div>13</div>
+  <div class="today">14</div>
+  <div>15</div>
+  <div>16</div>
+  <div>17</div>
+  <div>18</div>
+  <div>19</div>
+  <div>20</div>
+  <div>21</div>
+  <div>22</div>
+  <div>23</div>
+  <div>24</div>
+  <div>25</div>
+  <div>26</div>
+  <div>27</div>
+  <div>28</div>
+  <div>29</div>
+  <div>30</div>
+  <div>31</div>
+  <div></div>
+</div>
